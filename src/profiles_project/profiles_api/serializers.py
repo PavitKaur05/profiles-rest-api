128438ad73b5c402c131618b32febe3913ee32c8
Pavit Kaur <pkaur.2364@yahoo.com>
2020-03-10 01:41:28 +0530

    Added improved permissions to use profile feed items API

--- src/profiles_project/profiles_api/serializers.py
+++ src/profiles_project/profiles_api/serializers.py
@@ -10,7 +10,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model=models.UserProfile
-        fields=['id','email','name','password']
+        fields=('id','email','name','password')
         extra_kwargs={'password':{'write_only':True}}
 
 
@@ -21,3 +21,11 @@ class UserProfileSerializer(serializers.ModelSerializer):
         user.save()
 
         return user
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """A serializer for profile feed items"""
+
+    class Meta:
+        model=models.ProfileFeedItem
+        fields=('id','user_profile','status_text','created_on')
+        extra_kwargs={'user_profile':{'read_only':True}}
--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -8,6 +8,7 @@ router=DefaultRouter()
 router.register('hello-viewset',views.HelloViewSet,basename='hello-viewset')
 router.register('profile',views.UserProfileViewSet)# no need for base name as Django framework willautomatically figure it out for modelviewset
 router.register('login',views.LoginViewSet,basename='login')
+router.register('feed',views.UserProfileFeedViewSet)
 urlpatterns=[
 url(r'^hello-view/',views.HelloApiView.as_view()),
 url(r'',include(router.urls)) # empty string here so that url directs to this if initially specified not present
